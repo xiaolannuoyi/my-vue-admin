0552df1796f9b54b06bf4d7734196bf6b154965d
潇蓝诺依 <437963158@qq.com>
2019-11-16 22:54:21 +0800

    table整理

--- src/views/practice-center/table/tablebyall.vue
+++ src/views/practice-center/table/tablebyall.vue
@@ -4,21 +4,18 @@
     <my-search placeholder='请输入搜索内容' :Allresult='Allresult' :result.sync='result' searchKey='name'></my-search>
     <el-table :data="tableData" v-loading="loading" class="mytable" @select="handleSelectionChange"
         @select-all='handleSelectionChange' row-key="name">
-      <el-table-column type='selection' :reserve-selection='true' width="180"></el-table-column>
-      <el-table-column prop="date" label="日期" width="180"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column type='selection' :reserve-selection='true'></el-table-column>
+      <el-table-column prop="date" label="日期"></el-table-column>
+      <el-table-column prop="name" label="姓名"></el-table-column>
       <el-table-column prop="address" label="地址"></el-table-column>
     </el-table>
     <my-page ref='page' :result='result' :tableData.sync='tableData'></my-page>
-    <el-button @click="refresh">刷新</el-button>
   </div>
 </template>
 
 <script>
 import myPage from './page'
 import mySearch from './search'
-import { debug, debuglog } from 'util';
-import { constants } from 'crypto';
 export default {
     components:{
         myPage,mySearch
@@ -30,20 +27,6 @@ export default {
       Allresult:[],//所有数据
       loading: false,
       multipleSelection:[],
-      test:[
-        {
-          name:'刘三',
-          address:"北京"
-        },
-        {
-          name:'刘一',
-          address:"北京"
-        },
-        {
-          name:'刘二',
-          address:"北京"
-        }
-      ]
     };
   },
   methods: {
@@ -54,8 +37,6 @@ export default {
         this.loading = false;
 
         this.Allresult = this.result = data.result;
-        this.result.push(...this.test);
-      console.log('--333--',this.multipleSelection.length)
 
       });
     },
@@ -64,7 +45,6 @@ export default {
     },
     handleSelectionChange(selection, row) {
       this.multipleSelection = selection;
-      console.log('----',this.multipleSelection.length)
     },
   },
   beforeMount() {
